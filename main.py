25c89fdd8b6cbb5179917f51bf6c1c974176470f
Will Kencel <wkencel1@gmail.com>
2023-10-18 16:19:42 -0700

    added scraper function

--- main.py
+++ main.py
@@ -35,13 +35,13 @@ openai.api_key = OPENAI_KEY
 pinecone.init(api_key=PINECODE_KEY, environment=PINECODE_ENVIRONMENT)
 # uncomment these lines of code if it's your first time running. needed to create the pinecone index
 
-# # delete the pinecone index in case it already exists
-# pinecone.delete_index(PINECODE_DBNAME)
-# # create the pinecone index
-# pinecone.create_index(PINECODE_DBNAME, dimension=8, metric="euclidean")
-# #  print info of pinecone db
-# print(pinecone.list_indexes())
-# pinecone.describe_index(PINECODE_DBNAME)
+# delete the pinecone index in case it already exists
+pinecone.delete_index(PINECODE_DBNAME)
+# create the pinecone index
+pinecone.create_index(PINECODE_DBNAME, dimension=8, metric="euclidean")
+#  print info of pinecone db
+print(pinecone.list_indexes())
+pinecone.describe_index(PINECODE_DBNAME)
 
 vdb = pinecone.Index(index_name=PINECODE_DBNAME)
 
@@ -61,24 +61,24 @@ def read_root():
     return {
         "message": "Make a post request to /article to ask a question by virtual assistant"
     }
-# @app.post("/article")
-# def create_answer(params: RequestParams):
-#     question = params.article
-#     if question == '':
-#         return {"type": "error"}
-#     else:  
-#         vector = gpt3_embedding(question) # Get embedding for the question
-#         results = vdb.query(vector=vector, top_k=convo_length) # Find the closest vectors in the vector database
-#         articles = load_articles(results) # Load the articles related to those vectors
-#         prompt = PROMPT_RESPONSE.replace('<TITLE>', question).replace('<ARTICLE>', articles) # Formulate the prompt for GPT-3
-#         output = gpt3_completion(prompt) # Generate the response from GPT-3
-#         return { "answer": output }
+@app.post("/article")
+def create_answer(params: RequestParams):
+    question = params.article
+    if question == '':
+        return {"type": "error"}
+    else:  
+        vector = gpt3_embedding(question) # Get embedding for the question
+        results = vdb.query(vector=vector, top_k=convo_length) # Find the closest vectors in the vector database
+        articles = load_articles(results) # Load the articles related to those vectors
+        prompt = PROMPT_RESPONSE.replace('<TITLE>', question).replace('<ARTICLE>', articles) # Formulate the prompt for GPT-3
+        output = gpt3_completion(prompt) # Generate the response from GPT-3
+        return { "answer": output }
 
-# def load_articles(results):
-#     result = list()
-#     for m in results['matches']:
-#         info = load_data('%s/%s.db' % (DB_FOLDER, m['id'])) # Load the relevant articles
-#         result.append(info['article']) # Extract the article text
+def load_articles(results):
+    result = list()
+    for m in results['matches']:
+        info = load_data('%s/%s.db' % (DB_FOLDER, m['id'])) # Load the relevant articles
+        result.append(info['article']) # Extract the article text
 
 def extract_article_text_save_to_db():
   articles_urls = get_article_urls()
@@ -113,19 +113,19 @@ def create_db(params: DBParams):
   except:
     return { "status": "failed"}
 
-@app.post("/article")
-def article(params: RequestParams):
-  question = params.question
-  if question == '':
-    return { "type": "error" }
-  else:
-    vector = gpt3_embedding(question)
-    try:
-      results = vdb.query(vector=vector, top_k=convo_length, include_metadata=True)
-      uuid, answer = load_answer(results)
-      return {"status": "ok", "uuid": uuid, "answer": answer}
-    except:
-      return {"status": "failed"}
+# @app.post("/article")
+# def article(params: RequestParams):
+#   question = params.question
+#   if question == '':
+#     return { "type": "error" }
+#   else:
+#     vector = gpt3_embedding(question)
+#     try:
+#       results = vdb.query(vector=vector, top_k=convo_length, include_metadata=True)
+#       uuid, answer = load_answer(results)
+#       return {"status": "ok", "uuid": uuid, "answer": answer}
+#     except:
+#       return {"status": "failed"}
 
 def gpt3_embedding(content, engine='text-embedding-ada-002'):
   content = content.encode(encoding='ASCII',errors='ignore').decode()
